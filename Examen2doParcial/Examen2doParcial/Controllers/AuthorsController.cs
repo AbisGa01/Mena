cbe6fb725bd77ee6b217394bc7a5b647b48c3ba1
johnpeter101 <gutierreztorresjuanpedro@gmail.com>
2023-11-12 15:01:33 -0600

    primer commit

---
+++ Examen2doParcial/Examen2doParcial/Controllers/AuthorsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/authors")]
+public class AuthorsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AuthorsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+    {
+        return await _context.Authors.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Author>> GetAuthor(int id)
+    {
+        var author = await _context.Authors.FindAsync(id);
+
+        if (author == null)
+        {
+            return NotFound();
+        }
+
+        return author;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Author>> PostAuthor(Author author)
+    {
+        _context.Authors.Add(author);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+    }
+
+    // Otros métodos para actualizar y eliminar autores
+}
